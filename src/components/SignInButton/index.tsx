a9dd165f58ff00958b282674483201b235c5323d
Marcio Dauri <mdauri@gmail.com>
2022-05-03 13:31:13 -0300

    FaunaDB users and subscriptions

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,21 +1,26 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { useSession, signIn, signOut } from "next-auth/react"
 
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
-
-    return isUserLoggedIn ? (
+    const { data: session } = useSession()
+        
+    return session ? (
         <button type="button"
-        className={styles.signInButton}>
+        className={styles.signInButton}
+        onClick={() => signOut()}
+        >
             <FaGithub color="#04d361"/>
-            Márcio Daurí
+            {session.user.name}
             <FiX color="#737380" className={styles.closeIcon}/>
         </button>
     ):(
         <button type="button"
-        className={styles.signInButton}>
+        className={styles.signInButton}
+        onClick={() => signIn('github')}
+        >
             <FaGithub color="#eba417"/>
             Sign in with Github
         </button>
